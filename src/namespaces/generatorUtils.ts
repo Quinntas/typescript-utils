e98d71418b79ad70d36312e0728423e63decb92e
Caio "Quintas_" Santiago <13594102+Quinntas@users.noreply.github.com>
2024-06-11 12:43:44 -0300

    Update generatorUtils.ts

--- src/namespaces/generatorUtils.ts
+++ src/namespaces/generatorUtils.ts
@@ -1,3 +1,7 @@
 export namespace GeneratorUtils {
+    let GeneratorFunction = (function*(){yield undefined;}).constructor;
 
+    export function isGenerator<T>(value: any): value is Generator<T> {
+        return value instanceof GeneratorFunction
+    }
 }
